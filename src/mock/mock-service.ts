40304db76c9ef9017dd64deb69925d0d51b6695b
barisonurme <barisonurme@users.noreply.github.com>
2026-01-11 11:12:44 +0300

    feat: add @mui/lab dependency and project operation tab implemented

--- src/mock/mock-service.ts
+++ src/mock/mock-service.ts
@@ -14,66 +14,67 @@ export const getProjectList = (): Promise<TProjectsResponseFormatted[]> => {
 };
 
 // PROJECT DETAILS | Mock Service
-export const getProjectDetails = (projectId: string): Promise<TProject | undefined> => {
+export const getProjectDetails = (projectId: string): Promise<TProject | null> => {
 
     // TODO: Type assertion for mocked data
     const project = projectsMockData.find((p) => p.project_id === projectId) as TProject | undefined;
 
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(project);
+            resolve(project || null);
         }, 1500);
     });
 };
 
 // PROJECT TABLE DETAILS | Mock Service
-export const getProjectTableDetails = (projectId: string): Promise<ProjectTable[] | undefined> => {
+export const getProjectTableDetails = (projectId: string): Promise<ProjectTable[] | null> => {
 
     // TODO: Type assertion for mocked data
     const project = mockData.project_tables[projectId] as ProjectTable[] | undefined;
 
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(project);
+            resolve(project || null);
         }, 300);
     });
 };
 
 // PROJECT TABLE DETAILS | Mock Service
-export const getProjectRecentOperations = (projectId: string): Promise<RecentOperation[] | undefined> => {
+export const getProjectRecentOperations = (projectId: string): Promise<RecentOperation[] | null> => {
 
     // TODO: Type assertion for mocked data
     const project = mockData.recent_operations[projectId] as RecentOperation[] | undefined;
 
+
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(project);
+            resolve(project || null);
         }, 300);
     });
 };
 
 // PROJECT TABLE DETAILS | Mock Service
-export const getProjectGovernance = (projectId: string): Promise<ProjectGovernance | undefined> => {
+export const getProjectGovernance = (projectId: string): Promise<ProjectGovernance | null> => {
 
     // TODO: Type assertion for mocked data
     const project = mockData.governance[projectId] as ProjectGovernance | undefined;
 
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(project);
+            resolve(project || null);
         }, 300);
     });
 };
 
 // PROJECT TABLE DETAILS | Mock Service
-export const getProjectTableLineage = (projectId: string): Promise<TableLineageEntry[] | undefined> => {
+export const getProjectTableLineage = (projectId: string): Promise<TableLineageEntry[] | null> => {
 
     // TODO: Type assertion for mocked data
     const project = mockData.table_lineage[projectId] as TableLineageEntry[] | undefined;
 
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(project);
+            resolve(project || null);
         }, 300);
     });
 };
--- src/pages/projects/tabs/project-tab-operations.tsx
+++ src/pages/projects/tabs/project-tab-operations.tsx
@@ -1,6 +1,59 @@
+import ErrorComp from "@/components/organisms/error-comp"
+import LoadingComp from "@/components/organisms/loading-comp"
+import NoDataComp from "@/components/organisms/no-data-comp"
+import { useGetProjectRecentOperations } from "@/service/use-queries"
+import Timeline from "@mui/lab/Timeline"
+import TimelineConnector from "@mui/lab/TimelineConnector"
+import TimelineContent from "@mui/lab/TimelineContent"
+import TimelineDot from "@mui/lab/TimelineDot"
+import TimelineItem from "@mui/lab/TimelineItem"
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent"
+import TimelineSeparator from "@mui/lab/TimelineSeparator"
+import { Box, Card, CardContent, CardHeader, Divider, Typography } from "@mui/material"
+import { useParams } from "react-router"
+
+// ----------------------------------------------------------------------
+
 const ProjectTabOperations = () => {
+    const { projectId } = useParams<{ projectId?: string }>()
+    const { data, isLoading, isError, refetch } = useGetProjectRecentOperations(projectId ?? "")
+
+    if (isLoading) return <LoadingComp loadingText="Loading project details" />
+
+    if (isError) return <ErrorComp errorMessage="Error occurred while loading project details" onRefetch={refetch} />
+
     return (
-        <div>ProjectTabOperations</div>
+        <Box className='flex w-full h-full justify-start items-start'>
+            <Timeline className="flex w-full justify-start items-start">
+                {!data?.length && <Box className='flex w-full p-24'><NoDataComp onRefetch={refetch} /></Box>}
+                {data?.map((datum, index) => {
+                    const isLastIndex = data.length - 1 === index
+                    return <TimelineItem className="flex w-full" key={datum.operation_log_id}>
+                        <TimelineSeparator>
+                            <TimelineDot sx={{ backgroundColor: 'primary.main' }} />
+                            {!isLastIndex && <TimelineConnector />}
+                        </TimelineSeparator>
+                        <TimelineOppositeContent className="w-12 max-w-24 mt-2!">{datum.execution_timestamp ? new Date(datum.execution_timestamp).toLocaleDateString() : "--"}</TimelineOppositeContent>
+                        <TimelineContent className="flex w-full">
+                            <Card className="flex flex-col w-full">
+                                <CardHeader title={datum.affected_table} subheader={datum.output_table_version} />
+                                <CardContent className="flex flex-col gap-2">
+                                    <Box className='flex gap-2'>
+                                        <Typography>{datum.operation_name}</Typography>
+                                        <Typography>{datum.operation_type}</Typography>
+                                        <Typography>{datum.executed_by.name}</Typography>
+                                    </Box>
+                                    <Divider />
+
+                                    <Typography component="div">
+                                        {Object.entries(datum.input_parameters as Record<string, string>).map(([key, value], index, arr) => (<span key={key}><strong>{key}</strong>: {value}{index < arr.length - 1 && ", "}</span>))}
+                                    </Typography>
+                                </CardContent>
+                            </Card></TimelineContent>
+                    </TimelineItem>
+                })}
+            </Timeline>
+        </Box>
     )
 }
 
